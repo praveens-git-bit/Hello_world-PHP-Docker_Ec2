2997c8e8496438cd73e289e2b668623ce09afdd0
Praveen <99715700+praveens-git-bit@users.noreply.github.com>
2023-02-20 12:17:29 +0530

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,24 @@
+<?php
+echo "<html>
+        <head>
+            <title>Hello World</title>
+            <style>
+                body {
+                    background-color: #f0e68c;
+                    font-family: Arial, sans-serif;
+                    font-size: 18px;
+                    color: #006400;
+                }
+                h1 {
+                    color: #2f4f4f;
+                    font-size: 48px;
+                    text-align: center;
+                    margin-top: 100px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello, World!</h1>
+        </body>
+      </html>";
+?>
